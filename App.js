7510fe0be99298ef3ea590a642ec010c11633f5a
Gustavo Eidt <gustavo.eidt@gmail.com>
2021-05-15 13:25:36 -0300

    botao de porcentagem

--- App.js
+++ App.js
@@ -78,6 +78,9 @@ export default function App() {
         return;
       case '+/-':
         return;
+      case '%':
+        setCurrentNumber(currentNumber/100);
+        return;
     }
 
     setCurrentNumber(currentNumber + buttonPressed);
